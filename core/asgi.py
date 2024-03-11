18ec206d7bc2dff1aacacb283957c6d61fafd5f8
islam <dujsobaevislam01@gmail.com>
2024-03-11 23:51:02 +0600

    Initial commit

--- core/asgi.py
+++ core/asgi.py
@@ -1,11 +1,6 @@
 import os
-import sys
-
 from django.core.asgi import get_asgi_application
 
-path = '/home/islam24/djangoProject1/djangoProject1'
-if path not in sys.path:
-    sys.path.append(path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings.base')
 
--- core/wsgi.py
+++ core/wsgi.py
@@ -1,12 +1,7 @@
 import os
 import sys
-
 from django.core.wsgi import get_wsgi_application
 
-# Путь к вашему Django проекту
-path = '/home/islam24/djangoProject1/djangoProject1'
-if path not in sys.path:
-    sys.path.append(path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings.base')
 application = get_wsgi_application()
